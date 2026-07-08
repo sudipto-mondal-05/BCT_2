03207be7c73026b31c5bcd01700dca873d956d1c
sudipto-mondal-05 <sudiptohwh2005@gmail.com>
2026-07-08 11:11:45 +0530

    day3

--- day3.py
+++ day3.py
@@ -1,4 +1,8 @@
 import re
-txt = "hello planet"
-x = re.findall("he..o", txt)
+txt = "The rain in Spain falls mainly in the plain!"
+x = re.findall("falls|stays", txt)
 print(x)
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
